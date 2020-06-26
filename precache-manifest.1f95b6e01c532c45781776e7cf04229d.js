9d5e83532d4f45b28ba4ce2eda464aa24cf2019f
tahiri-78 <66792921+tahiri-78@users.noreply.github.com>
2020-06-26 02:00:26 +0100

    Updates

---
+++ precache-manifest.1f95b6e01c532c45781776e7cf04229d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bef41f25c996114e8188e69fdf0219",
+    "url": "/robotfriends_F/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotfriends_F/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e3ab27dadfccbc46ef1",
+    "url": "/robotfriends_F/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotfriends_F/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends_F/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3ab27dadfccbc46ef1",
+    "url": "/robotfriends_F/static/js/main.25798859.chunk.js"
+  },
+  {
+    "revision": "af40d86cc9cece02438c",
+    "url": "/robotfriends_F/static/js/runtime-main.739e71d0.js"
+  }
+]);
